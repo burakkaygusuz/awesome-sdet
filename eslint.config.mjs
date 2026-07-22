742cc6a4b8c43f4465cc4fa2846fb3e362f199d9
Burak Kaygusuz <burakkaygusuz@outlook.com>
2026-07-23 00:36:34 +0300

    feat(formatting): add prettier and eslint flat configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import markdown from '@eslint/markdown';
+import markdownlint from 'eslint-plugin-markdownlint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    ...js.configs.recommended,
+  },
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.md'],
+    plugins: {
+      markdown,
+      markdownlint,
+    },
+    language: 'markdown/commonmark',
+    rules: {
+      'markdownlint/md001': 'error',
+      'markdownlint/md022': 'error',
+    },
+  },
+  eslintConfigPrettier,
+]);
