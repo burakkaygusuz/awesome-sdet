e0589a30b2f657d7ee046ffef68f00bc585d74c3
Burak Kaygusuz <burakkaygusuz@outlook.com>
2026-08-01 12:45:38 +0300

    build(workspace): add servers package to pnpm workspace, configure lint-staged.config.mjs, update packages, and set @ path alias

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
   {
-    ignores: ['dist/**', 'node_modules/**'],
+    ignores: ['**/dist/**', '**/node_modules/**'],
   },
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
